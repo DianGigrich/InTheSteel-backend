e7bcb0cae8be6d92676e3b5f53b9db66da94ff84
Dian Sherman <slayer_barrett_@hotmail.com>
2023-10-19 11:10:44 -0700

    added server file

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const routes = require('./routes');
+
+// Import the connection object
+const sequelize = require('./config/connection');
+
+
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const cors = require("cors")
+app.use(cors())
+// turn on routes
+app.use(routes);
+
+// Connect to the database before starting the Express.js server
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
